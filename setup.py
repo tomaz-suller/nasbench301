5c4749c1f46eeb6789fa041a23e46f031c04e466
tomaz-suller <tomaz.suller@usp.br>
2024-08-19 19:47:27 +0200

    Move Git requirements to `setup.py`

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 import os
 import sys
 import setuptools
+import subprocess
+from importlib import import_module
+
+
+def install_if_not_found(module: str, install_from: str, user: bool = False):
+    try:
+        import_module(module)
+    except ModuleNotFoundError:
+        print(f"Installing {module}")
+        python_command = f"{sys.executable} -m pip install --upgrade".split()
+        if user:
+            python_command.append("--user")
+        python_command.append(install_from)
+        subprocess.run(python_command, check=False)
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -9,6 +24,11 @@ requirements = []
 with open('requirements.txt', 'r') as f:
     for line in f:
         requirements.append(line.strip())
+
+install_if_not_found(
+    "ConfigSpace",
+    "git+https://github.com/tomaz-suller/ConfigSpace.git@fix-naslib",
+)
 
 #optional_requirements = []
 #with open('optional-requirements.txt', 'r') as f:
